52ff14dd8f849f508f1238fb13d3856422bf3f71
Matt Amacker <matt@soovle.com>
2018-03-15 06:19:11 +0000

    Now with working sense hat control.

--- sense-hat/robotio-sense.py
+++ sense-hat/robotio-sense.py
@@ -52,20 +52,20 @@ def get_board(id):
         for x in range(len(board_raw)):
             if board_raw[x] is not None:
                 for y in range(len(board_raw[x])):
-                    found = False;
                     if board_raw[x][y] is not None:
-                        if board_raw[x][y].get("visited") is not None:
-                            if board_raw[x][y].get("visited") is True:
+                        if board_raw[x][y].get("safe") is not None:
                                 if board_raw[x][y].get("safe"):
-                                    board[board_raw[x][y].get("x")][board_raw[x][y].get("y")] = 1
+                                    if board_raw[x][y].get("visited") is True:
+                                        board[board_raw[x][y].get("x")][board_raw[x][y].get("y")] = 1
                                 else:
                                     board[board_raw[x][y].get("x")][board_raw[x][y].get("y")] = -1
-                            if max_size < board_raw[x][y].get("y"):
-                                max_size = board_raw[x][y].get("y")
+                        if max_size < board_raw[x][y].get("y"):
+                            max_size = board_raw[x][y].get("y")
 
         # Trim the board to the maximum y value seen.
         for j in range(len(board)):
-            board[j] = board[j][:max_size]
+            for k in range(max_size, 20):
+                board[j][k] = 1
 
         position = [json_data["position"].get("x"), json_data["position"].get("y")];
         changed = True
@@ -75,18 +75,24 @@ sense = SenseHat()
 sense.clear()
 
 while True:
-    board, position, level, changed = get_board('qmda8df2')
+    board, position, level, changed = get_board('qmda82')
+    shift = 1
     if changed is True:
         sense.clear()
-        for x in range(board):
-            for y in range(board[x]):
+        for x in range(len(board)):
+            shift_x = x - shift
+            if shift_x > 7 or shift_x < 0:
+                continue
+            for y in range(len(board[x])):
+                if y > 7:
+                    continue
                 if board[x][y] == 1:
-                    sense.set_pixel(x, y, [0, 255, 0])
+                    sense.set_pixel(shift_x, 7 - y, [0, 255, 0])
                 elif board[x][y] == -1:
-                    sense.set_pixel(x, y, [255, 0, 0])
+                    sense.set_pixel(shift_x, 7 - y, [255, 0, 0])
                 elif board[x][y] == 0:
-                    sense.set_pixel(x, y, [0, 0, 0])
+                    sense.set_pixel(shift_x, 7 - y, [0, 0, 0])
 
-        sense.set_pixel(position[0], position[1])
+        sense.set_pixel(position[0] - shift, 7 - position[1], [0, 0, 255])
     time.sleep(1)
 
